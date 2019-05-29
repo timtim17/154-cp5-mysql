2c284ab5c69ebb28a68ac5f5f238ce5c2a95cadc
Austin Jenchi <ajenchi9@gmail.com>
2019-05-29 15:20:21 -0700

    add a few different characters

--- main.js
+++ main.js
@@ -20,33 +20,47 @@
         canvas.width = parseInt(canvasCompStyles.width);
         canvas.height = parseInt(canvasCompStyles.height);
 
+        let chars = [];
+
         let linkImg = new Image();
         linkImg.src = "img/sprites/link.png";
         linkImg.addEventListener("load", () => {
-            let link = new Character(linkImg, 41, 48, 41, 83, 79, 48, 4, 4, 3, 66, 194, 322, 62, 45, 49, 82);
-            setInterval(render, 200, ctx, link);
-            render(ctx, link);
+            chars.push(new Character(linkImg, 41, 48, 41, 83, 73, 48, 4, 4, 3, 66, 194, 322, 62, 45, 49, 82));
         });
+
+        let pikaImg = new Image();
+        pikaImg.src = "img/sprites/pikachu.png";
+        pikaImg.addEventListener("load", () => {
+            chars.push(new Character(pikaImg, 39, 34, 39, 85, 69, 27, 4, 4, 3, 83, 211, 339, 45, 43, 58, 88));
+        });
+
+        let corrinImg = new Image();
+        corrinImg.src = "img/sprites/corrin.png";
+        corrinImg.addEventListener("load", () => {
+            chars.push(new Character(corrinImg, 40, 25, 40, 86, 62, 15, 4, 4, 3, 59, 187, 315, 69, 42, 66, 99));
+        });
+
+        setInterval(render, 100, ctx, chars);
+        render(ctx, chars);
     }
 
-    let i = 0;
 
+    let t = 0;
     /**
      * 
      * @param {*} ctx 
-     * @param {Character} char 
+     * @param {Character[]} chars 
      */
-    function render(ctx, char) {
+    function render(ctx, chars) {
         ctx.fillStyle = "#ffffff";
-        ctx.imageSmoothingEnabled = false;
         ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        let curSprite = char.sprites.idle[i];
-        ctx.drawImage(char.spritesheet, curSprite.x, curSprite.y, curSprite.width, curSprite.height, 100, 100, curSprite.width * 2.5, curSprite.height * 2.5);
-        if (i >= char.sprites.idle.length - 1) {
-            i = 0;
-        } else {
-            i++;
-        }
+        ctx.imageSmoothingEnabled = false;
+        let i = 0;
+        let y = 200 * Math.sin(t) + 400;
+        t += 0.025;
+        chars.forEach(char => char.drawSprite(ctx, 200 + 200 * i++, y));
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(0, y, ctx.canvas.width, 50);
     }
 
     /**
@@ -84,7 +98,31 @@
                 splitSpritesheet(moveOffset, moveShift, numMove, yMove, moveWidth, height);
             this.sprites[STATE_ATK] =
                 splitSpritesheet(atkOffset, atkShift, numAtk, yAtk, atkWidth, height);
-            this.state = "idle";
+            this.state = STATE_MOVE;
+            this.curSprite = 0;
+        }
+
+        /**
+         * 
+         * @param {*} newState 
+         */
+        setState(newState) {
+            this.state = newState;
+        }
+
+        /**
+         * 
+         * @param {*} ctx 
+         */
+        drawSprite(ctx, x, y) {
+            let curSprite = this.sprites[this.state][this.curSprite];
+            ctx.drawImage(this.spritesheet, curSprite.x, curSprite.y, curSprite.width,
+                curSprite.height, x, y - curSprite.height * 2.5, curSprite.width * 2.5, curSprite.height * 2.5);
+            if (this.curSprite >= this.sprites[this.state].length - 1) {
+                this.curSprite = 0;
+            } else {
+                this.curSprite++;
+            }
         }
     }
 
@@ -102,7 +140,6 @@
     function splitSpritesheet(offset, shift, num, y, width, height) {
         let res = [];
         for (let i = 0; i < num; i++) {
-            console.log(res);
             res.push({
                 x: offset + i * (width + shift),
                 y: y,
